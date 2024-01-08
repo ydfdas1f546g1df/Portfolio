7cd9cd636c7603254d14c42009a83d89257ec060
Niklaus Kuelling <niklauskuelling@gmx.ch>
2024-01-08 18:17:14 +0100

    Maybe next time

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
-
-
-export const prerender = true;
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { prerender } from './src/routes/+layout';
 
 /** @type {import('@sveltejs/kit').Config} */
-export default {
-    preprocess: sveltePreprocess(),
-    kit: {
-        adapter: adapter()
-    },
+const config = {
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
-
+export const prerender = true;
+export default config;
